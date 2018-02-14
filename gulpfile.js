329e86674bc56601936704612eb8e73ba011282a
tbcrawford <crawfordt.014@gmail.com>
2018-02-14 11:53:39 -0500

    Added csscomb and eslint. Enabled minified output of flex form in gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
+/*global require */
 var gulp = require('gulp');
+var rename = require('gulp-rename');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function() {
@@ -10,7 +12,17 @@ gulp.task('sass', function() {
         .pipe(gulp.dest('dist/flex-form/0.2.0'));
 });
 
+gulp.task('sass-min', function() {
+    return gulp.src('src/scss/flex.form.scss')
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }).on('error', sass.logError))
+        .pipe(rename('flex.form.min.css'))
+        .pipe(gulp.dest('dist/flex-form/0.2.0'));
+});
+
 gulp.task('watch', function(done) {
     gulp.watch('src/scss/**/*.scss', gulp.series('sass'));
+    gulp.watch('src/scss/**/*.scss', gulp.series('sass-min'));
     done();
 });
